9680d711e7f421df6ccec5732eab556419005786
damienbod <damien_bod@hotmail.com>
2016-12-12 18:33:54 +0100

    Startup configuration

---
+++ src/EFCoreFeaturesAndDiag/Startup.cs
@@ -0,0 +1,54 @@
+﻿using EFCoreFeaturesAndDiag.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace EFCoreFeaturesAndDiag
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var sqlConnectionString = Configuration.GetConnectionString("DataAccessMsSqlServerProvider");
+
+            services.AddDbContext<DomainModelMsSqlServerContext>(
+                options => options.UseSqlServer(
+                    sqlConnectionString,
+                    sqlServerOptions => sqlServerOptions.EnableRetryOnFailure()
+                )
+            );
+
+            services.AddScoped<IDataAccessProvider, DataAccessMsSqlServerProvider>();
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
