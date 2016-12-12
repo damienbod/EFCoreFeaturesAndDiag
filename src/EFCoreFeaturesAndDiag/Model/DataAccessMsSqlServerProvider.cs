7a1f6ec54b70f8960338730638a3ae3e7294dd4d
damienbod <damien_bod@hotmail.com>
2016-12-12 18:32:02 +0100

    Adding EFCore model and context

---
+++ src/EFCoreFeaturesAndDiag/Model/DataAccessMsSqlServerProvider.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EFCoreFeaturesAndDiag.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EFCoreFeaturesAndDiag.Model
+{
+    public class DataAccessMsSqlServerProvider : IDataAccessProvider
+    {
+        private readonly DomainModelMsSqlServerContext _context;
+        private readonly ILogger _logger;
+
+        public DataAccessMsSqlServerProvider(DomainModelMsSqlServerContext context, ILoggerFactory loggerFactory)
+        {
+            _context = context;
+            loggerFactory.AddProvider(new EfCoreLoggerProvider());
+            _logger = loggerFactory.CreateLogger("DataAccessMsSqlServerProvider");
+
+        }
+
+        public void AddDataEventRecord(DataEventRecord dataEventRecord)
+        {
+            if (dataEventRecord.SourceInfo != null && dataEventRecord.SourceInfoId == 0)
+            {
+                _context.SourceInfos.Add(dataEventRecord.SourceInfo);
+            }
+            else
+            {
+                var sourceInfo = _context.SourceInfos.Find(dataEventRecord.SourceInfo.SourceInfoId);
+                sourceInfo.Description = dataEventRecord.MadDescription;
+                sourceInfo.Name = dataEventRecord.Name;
+                dataEventRecord.SourceInfo = sourceInfo;
+            }
+
+            _context.DataEventRecords.Add(dataEventRecord);
+            _context.SaveChanges();
+        }
+
+        public void UpdateDataEventRecord(long dataEventRecordId, DataEventRecord dataEventRecord)
+        {
+            _context.DataEventRecords.Update(dataEventRecord);
+            _context.SaveChanges();
+        }
+
+        public void DeleteDataEventRecord(long dataEventRecordId)
+        {
+            var entity = _context.DataEventRecords.Find(dataEventRecordId);
+            _context.DataEventRecords.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public DataEventRecord GetDataEventRecord(long dataEventRecordId)
+        {
+            return _context.DataEventRecords.Find(dataEventRecordId);
+        }
+
+        public List<DataEventRecord> GetDataEventRecords()
+        {
+            // Using the shadow property EF.Property<DateTime>(dataEventRecord)
+            return _context.DataEventRecords.OrderByDescending(dataEventRecord => EF.Property<DateTime>(dataEventRecord, "UpdatedTimestamp")).ToList();
+        }
+
+        public List<SourceInfo> GetSourceInfos(bool withChildren)
+        {
+            // Using the shadow property EF.Property<DateTime>(srcInfo)
+            if (withChildren)
+            {
+                return _context.SourceInfos.Include(s => s.DataEventRecords).OrderByDescending(srcInfo => EF.Property<DateTime>(srcInfo, "UpdatedTimestamp")).ToList();
+            }
+
+            return _context.SourceInfos.OrderByDescending(srcInfo => EF.Property<DateTime>(srcInfo, "UpdatedTimestamp")).ToList();
+        }
+    }
+}
